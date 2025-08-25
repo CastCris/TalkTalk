1e5f0a02b7eeb9961b4aa24fb5ef152fb1cec1f3
Cristiano <cristianochincarosanches@gmail.com>
2025-08-25 08:13:13 -0300

    change_the_aim--using_socetio_and_flask--_aim:discord_2.0

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import flask
+import socketio
+import eventlet
+import eventlet.wsgi
 
-app=flask.Flask(__name__)
+sio = socketio.Server()
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index()->object:
     return flask.render_template('/index.html')
 
+@sio.event
+def connect(sid, environ):
+    print('Client {} connect'.format(sid))
+
+@sio.event
+def disconnect(sid, data):
+    print('Client {} disconnet'.format(sid))
+
+eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+
+"""
 if __name__=='__main__':
     app.run(debug=True)
+"""
